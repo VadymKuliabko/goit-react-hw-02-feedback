b840c1eec8877d13b66693f1fc4194c5986188b7
Vadym-Kuliabko <vadilatele@gmail.com>
2022-04-20 14:50:24 +0200

    Update FeedbackOption.styled

---
+++ src/components/FeedbackOptions/FeedbackOption.styled.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const WrapperBtn = styled.div`
+  width: 500px;
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+export const Button = styled.button`
+  padding: 10px 50px 10px 50px;
+  border: none;
+  border-radius: 10px;
+  text-transform: capitalize;
+  font-size: 20px;
+  cursor: pointer;
+`;
